aca26ad9c6650cf07bbee4551383c744d2c2c4b1
Devanshi Kakadiya <devanshikakadiya02@gmail.com>
2024-09-10 22:59:59 +0530

    set root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_scope) = new_order_path
+
   protected
 
   def configure_permitted_parameters
